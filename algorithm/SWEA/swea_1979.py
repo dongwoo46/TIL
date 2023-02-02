757f0d067763d1260dfd34f318c852072689d95b
김동우 <qwa406@naver.com>
2023-02-02 12:03:51 +0900

    counting_sort

--- algorithm/SWEA/swea_1979.py
+++ algorithm/SWEA/swea_1979.py
@@ -7,11 +7,10 @@ for test_case in range(1,T+1):
     board = []
     one_count_length = []
     one_count_width = []
-    n, m = map(int,input().split())
-
+    n, m = map(int, input().split())
 
     for j in range(n):
-        board.append(list(map(int,input().split())))
+        board.append(list(map(int, input().split())))
 
 # board = [input().split() for _ in range(n)]
 
@@ -23,10 +22,9 @@ for test_case in range(1,T+1):
             else:
                 one_count = 0
             one_count_width.append(one_count)
-            
-            
+
     for i in one_count_width:
-        if i>=m:   
+        if i >= m:
             if i == m:
                 cnt += 1
             elif i == m+1 :
@@ -34,8 +32,6 @@ for test_case in range(1,T+1):
         else:
             continue
 
-         
-            
     for i in range(n):
         one_count = 0
         for j in range(n):
@@ -45,17 +41,15 @@ for test_case in range(1,T+1):
                 one_count = 0
             one_count_length.append(one_count)
 
-
     for i in one_count_length:
-        if i>=m:   
+        if i >= m:
             if i == m:
                 cnt += 1
             elif i == m+1 :
                 cnt -=1
         else:
             continue 
-            
-            
+
     print(f'#{test_case} {cnt}')
     
 
