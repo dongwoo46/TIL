4755bd692eb3632270bbba0318f151bf28ee6215
김동우 <qwa406@naver.com>
2023-02-14 02:23:53 +0900

    20230214_0223

--- 개인 알고리즘 공부/backjoon/20230213/체스판다시칠하기_1018.py
+++ 개인 알고리즘 공부/backjoon/20230213/체스판다시칠하기_1018.py
@@ -11,27 +11,30 @@ for i in range(n):
             board[i][j] = 0
         else:
             board[i][j] = 1
-eight_list = []
-cnt_list = []
+min_cnt = 100000000
 #8*8행렬로 큰 행렬을 짜르고 만약 그 안의 합이 32에 가장 가까운것을 고름
-for i in range(n):
-    for j in range(m):
-        eight_board = [[0] * 8 for _ in range(8)]
+for i in range(n-8+1):
+    for j in range(m-8+1):
+        eight_board = []
         cnt = 0
-        if i%8==0 and j%8==0:
-            for k in range(i,i+8):
-                for l in range(j,j+8):
-                    eight_board[i][j] = board[i][j]
-            for row in eight_board:
-                cnt += sum(row)
-            cnt_list.append(cnt)
+        for k in range(i,i+8):
+            for l in range(j,j+8):
+                # eight_board.append(board[k][l])
+                if board[k][l] == 1:
+                    cnt+=1
+        # cnt = sum(eight_board)
+
+        if abs(cnt -32) < min_cnt:
+            min_cnt = abs(cnt-32)
+
+print(min_cnt)
 
 
 
 
 
-for row in eight:
-    cnt += sum(row)
+
+
 
 
 
