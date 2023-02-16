aeed7bac5236f7c5496101d2454d1eeb791c3f51
김동우 <qwa406@naver.com>
2023-02-16 20:32:44 +0900

    20230216_2032

--- 개인 알고리즘 공부/backjoon/20230213/오리_12933.py
+++ 개인 알고리즘 공부/backjoon/20230213/오리_12933.py
@@ -5,39 +5,36 @@ cry = list(input())
 cnt=0
 stack1 = []
 quack = [0] * len(cry)
-while True:
-    if sum(quack) == len(cry):
-        break
+
+def duck(start):
+    global cnt
+    first = 0
 
     for i in range(len(cry)):
-        stack1.clear()
-        if cry[i] == 'q':
-            if quack[i] !=0:
-                a = i
+        if cry[i] == 'q' and quack[i] == 0:
+            stack1.append(cry[i])
+            quack[i] = 1
+            # previous = cry[i]
+        elif stack1[-1] == 'q'  and  cry[i] == 'u':
+            if quack[i] == 0:
+                quack[i] = 1
                 stack1.append(cry[i])
-        elif 'q' in stack1 and  cry[i] == 'u':
-            if quack[i] != 0:
+
+        elif stack1[-1] == 'u' and cry[i] == 'a':
+            if quack[i] == 0:
                 stack1.append(cry[i])
-                b = i
-        elif 'u' in stack1 and cry[i] == 'a':
-            if quack[i] != 0:
-                stack1.append(cry[i])
+                quack[i]
                 c = i
         elif 'a' in stack1 and cry[i] == 'c':
-            if quack[i] != 0:
+            if quack[i] == 0:
                 stack1.append(cry[i])
                 d = i
         elif 'c' in stack1 and cry[i] == 'k':
-            if quack[i] != 0:
+            if quack[i] == 0:
                 stack1.append(cry[i])
                 e = i
-        if ''.join(stack1) == 'quack':
-            quack[a] = 1
-            quack[b] = 1
-            quack[c] = 1
-            quack[d] = 1
-            quack[e] = 1
-            cnt += 1
+    if stack1:
+        cnt += 1
         # elif len(stack1) !=0:
         #     if stack1[-1] == 'q' and a == 'u':
         #         stack1.append(a)
