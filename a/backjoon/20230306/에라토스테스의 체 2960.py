b268f259c07ffb5d01dcd5f742844e496ba08325
김동우 <qwa406@naver.com>
2023-03-07 01:34:04 +0900

    '20230307_0133'

---
+++ a/backjoon/20230306/에라토스테스의 체 2960.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('input','r')
+n , k = list(map(int,input().split()))
+ans = 0
+board = [True]*(n+1)
+cnt = 0
+for i in range(2,n+1):
+    for j in range(i, n+1,i):
+        if board[j] == True:
+            board[j] = False
+            cnt += 1
+            if cnt == k:
+                ans = j
+                break
+
+print(ans)
