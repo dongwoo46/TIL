a131b14b1e407ff774017ec895ee83a669022385
김동우 <qwa406@naver.com>
2023-03-06 18:00:55 +0900

    '20230306_1800'

--- a/backjoon/20230306/3085.py
+++ a/backjoon/20230306/3085.py
@@ -1,35 +1,34 @@
-import sys
-sys.stdin = open('input','r')
-n = int(input())
-board = [list(input()) for _ in range(n)]
 
-delta = [(1,0),(0,1)]
-delta_r = [(-1,0),(1,0),(0,-1),(0,1)]
 
 def is_valid(x,y):
     return 0<=x<n and 0<=y<n
 
-max_cnt = 0
+def change_board(board):
+    mx = 0 
+    for x in range(n-1):
+        for y in range(n):
+            for dx,dy in delta:
+                nx = x + dx
+                ny = y + dy
+                if is_valid(nx,ny):
+                    board[x][y], board[nx][ny] = board[nx][ny],board[x][y]
+                    mx = max(mx,check_row(board[x]),check_row(board[x+1]))
+                    board[nx][ny], board[x][y] = board[x][y],board[nx][ny]
+    return mx
 
-for x in range(n):
-    for y in range(n):
-        
-        for dx,dy in delta:
-            nx = x + dx
-            ny = y + dy
-            if is_valid(nx,ny):
-                board[x][y],board[nx][ny] = board[nx][ny],board[x][y]
-            for dx, dy in delta_r:
-                cnt = 0
-                for i in range(n):
-                    nx = x + dx*i
-                    ny = y + dy*i
-                    if is_valid(nx,ny) and board[x][y] == board[nx][ny]:
-                        cnt +=1
-                    else:
-                        continue
-                max_cnt = max(max_cnt,cnt)
-            if is_valid(nx,ny):
-                board[nx][ny], board[x][y]  = board[x][y], board[nx][ny]
+def check_row(lst):
+    cnt = ans = 1
+    for i in range(1,len(lst)):
+        if lst[i] == lst[i-1]:
+            cnt += 1
+            ans = max(ans,cnt)
+    return ans
 
-print(max_cnt)
+n = int(input())
+board = [list(input()) for _ in range(n)]
+board_t = list(map(list,zip(*board)))
+# board_t = [[board[j][i] for j in range(n)] for i in range(n)]
+
+delta = [(1,0),(0,1)]
+ans = max(change_board(board),change_board(board_t))
+print(ans)
