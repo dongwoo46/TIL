043907ce63b4f09d47e6c3eb68f187850fe511db
김동우 <qwa406@naver.com>
2023-03-30 07:41:19 +0900

    20230330_0741

---
+++ 개인 알고리즘 공부/backjoon/20230329/test.py
@@ -0,0 +1,2 @@
+board = [[0]*5 for _ in range(5)]
+print(board[0:3][0:3])
--- 개인 알고리즘 공부/backjoon/20230329/색종이 만들기 2630.py
+++ 개인 알고리즘 공부/backjoon/20230329/색종이 만들기 2630.py
@@ -1,5 +1,34 @@
 # 백준 색종이 만들기 2630
 
 import sys
+from collections import deque
 sys.stdin = open('input','r')
 
+def divide(board):
+    if m <=1:
+        return m
+
+    mid = len(m)//2
+    d1 = board[:mid][:mid]
+    d2 = board[mid:][:mid]
+    d3 = board[mid:]
+
+
+def bfs(x,y,l):
+    q = deque()
+    q.append((x,y,l))
+
+    visited[x][y] = board[x][y]
+    while q:
+        x, y, l = q.popleft()
+        for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]
+            nx = x + dx
+            ny = y + dy
+            if 0<=nx<l and 0<=ny<l and visited[nx][ny]==-1:
+                q.append((nx,ny))
+                visited[nx][ny] = board[nx][ny]
+
+
+n = int(input())
+board = [list(map(int,input().split())) for _ in range(n)]
+visited = [[-1]*l for _ in range(l)] # 사각형의 길이
