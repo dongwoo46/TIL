e8d3e5a9029fd20cb0b1c6698ae40768a3fd9d56
김동우 <qwa406@naver.com>
2023-04-06 01:17:19 +0900

    20230406_0117

--- 개인 알고리즘 공부/backjoon/20230405/backjoon 패션왕 신해빈 9375.py
+++ 개인 알고리즘 공부/backjoon/20230405/backjoon 패션왕 신해빈 9375.py
@@ -1,24 +1,17 @@
 import sys
 sys.stdin = open('input','r')
 
-def backtrack(v,lst):
-    global cnt
-    if v == len(genre):
-        for i in range(len(lst)-1):
-            if lst[i] == lst[i+1]:
-                return
-            else:
-                cnt+=1
-    backtrack(v+1, lst+[genre[v]])
-    backtrack(v+1, lst)
-
 T = int(input())
 for tc in range(1,T+1):
     n= int(input())
-    fashion = [input().split() for _ in range(n)]
-    cnt = 0
-    genre = []
+    dic = {}
     for i in range(n):
-        genre.append(fashion[i][1])
-    backtrack(0,[])
-    print(cnt)
+        style, body = input().split()
+        if body not in dic:
+            dic[body] = 1
+        else:
+            dic[body] +=1
+    ans = 1
+    for item in dic.values():
+        ans*=item+1
+    print(ans-1)
