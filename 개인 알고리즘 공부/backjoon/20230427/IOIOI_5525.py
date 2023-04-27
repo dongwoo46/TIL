d8624cc7a1ae4ded7fa8aa4849eeef12124901d7
kimdongwoo <siwol406@gmail.com>
2023-04-27 17:59:37 +0900

    '20230427_1800'

---
+++ 개인 알고리즘 공부/backjoon/20230427/IOIOI_5525.py
@@ -0,0 +1,53 @@
+
+import sys
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+s = input()
+
+lst = [0]*(1000002)
+v = 1000001
+# n == 1일때는 따로 설정해야함 'IOI'일때 따로 설정 즉 O가 1개일때
+for i in range(1,1000001):
+    lst[i] = v
+    v-=1
+# p = 'IOI'+(n-1)*'OI'
+cnt = 0
+result = []
+result_cnt = []
+string = ''
+for i in s:
+    if len(string) == 0:
+        if i == 'I':
+            string+=i
+    else:
+        if string[-1] != i:
+            string+=i
+            if i == 'O':
+                cnt+=1
+        else:
+            result.append(string)
+            result_cnt.append(cnt-n+1)
+            string = ''
+            cnt = 0
+            if i == 'I':
+                string+=i
+else:
+    result.append(string)
+    result_cnt.append(cnt-n+1)
+
+print(result)
+print(result_cnt)
+
+print(sum(result_cnt))
+# OOIOIOIOIIOIIOIOIOIO
+# end = len(p)-1
+# for start in range(m-len(p)+1):
+#
+#     if s[start:end+1] == p:
+#         cnt+=1
+#     end +=1
+#
+# print(cnt)```
